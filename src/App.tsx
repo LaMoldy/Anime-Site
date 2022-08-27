c44a83c5dd4d31d457f75dddfcf071c5806c4f6d
Jax <jaxdrummond9@gmail.com>
2022-08-27 13:46:25 -0400

    Added auto code formatter, and some slight changes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const element = <h1>HelloWorld</h1>;
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      {element}
       <h3>Nikk is stupid</h3>
+      <h1>Goodbye World</h1>
+      <button>Rip</button>
     </div>
   );
 }
