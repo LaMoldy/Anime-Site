9772ea9e7a7a006e4d512a8f310ede6e3a112346
Nikk Jackson <NikkolasJ@Outlook.com>
2022-08-28 09:41:49 -0400

    fix: changed everything to 4 space indents

--- src/App.tsx
+++ src/App.tsx
@@ -5,76 +5,74 @@ import {MALAnime} from "./interfaces/malTypes";
 import {AnimeCard} from "./components/anime-card";
 
 function App() {
-  const [searchedAnime, setSearchedAnime] = useState<MALAnime[]>([]);
-  const [input, setInput] = useState("");
-  const [searching, setSearching] = useState(false);
-  const [animeList, setAnimeList] = useState<MALAnime[]>([]);
+    const [searchedAnime, setSearchedAnime] = useState<MALAnime[]>([]);
+    const [input, setInput] = useState("");
+    const [searching, setSearching] = useState(false);
+    const [animeList, setAnimeList] = useState<MALAnime[]>([]);
 
-  const fetchData = useCallback(async () => {
+    const fetchData = useCallback(async () => {
       const animes = await Mal.getAllAnime();
       setAnimeList(animes);
-  }, [setAnimeList]);
+    }, [setAnimeList]);
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
 
-  function searchAnime() {
-    if (input === "") {
-      setSearching(false);
-    } else {
-      setSearching(true)
-      Mal.getAnime(input).then(data => {
-        setSearchedAnime(data);
-      });
+    function searchAnime() {
+        if (input === "") {
+          setSearching(false);
+        } else {
+          setSearching(true)
+          Mal.getAnime(input).then(data => {
+            setSearchedAnime(data);
+          });
+        }
     }
-  }
 
-  function updateInput(event: ChangeEvent<HTMLInputElement>) {
-    setInput(event.target.value);
-  }
+    function updateInput(event: ChangeEvent<HTMLInputElement>) {
+        setInput(event.target.value);
+    }
 
-  return (
-    <div className={"background"}>
-      <div className={"flex-column center"}>
-        <h1 className={"page-title"}>Ani-Hub</h1>
-        <input
-          className={"home-search-bar"}
-          onChange={updateInput}
-          type="text"
-          placeholder="Anime Name"/>
-        <button
-          onClick={searchAnime}
-          className={"search-button"}>
-          Search
-        </button>
-      </div>
-      <br></br>
-      <br></br>
-      {searching &&
-          <div className="flex-row space-evenly anime-list">
-            {searchedAnime.map(data =>
-              <AnimeCard
-                image={data.images.jpg.image_url}
-                title={data.title_english}
-                key={data.mal_id}
-              />
-            )}
-          </div>
-      }
-      {!searching &&
-          <div className="flex-row space-evenly anime-list">
-            {animeList.map(data =>
-              <AnimeCard
-                image={data.images.jpg.image_url}
-                title={data.title_english}
-                key={data.mal_id}
-              />
-            )}
-          </div>
-      }
-    </div>
-  );
+    return (
+        <div className={"background"}>
+          <div className={"flex-column center"}>
+            <h1 className={"page-title"}>Ani-Hub</h1>
+            <input
+                className={"home-search-bar"}
+                onChange={updateInput}
+                type="text"
+                placeholder="Anime Name"/>
+            <button
+                onClick={searchAnime}
+                className={"search-button"}>
+                Search
+            </button>
+        </div>
+        <br></br>
+        <br></br>
+        {searching &&
+            <div className="flex-row space-evenly anime-list">
+                {searchedAnime.map(data =>
+                    <AnimeCard
+                        image={data.images.jpg.image_url}
+                        title={data.title_english}
+                        key={data.mal_id} />
+                )}
+            </div>
+          }
+          {!searching &&
+            <div className="flex-row space-evenly anime-list">
+                {animeList.map(data =>
+                <AnimeCard
+                    image={data.images.jpg.image_url}
+                    title={data.title_english}
+                    key={data.mal_id} />
+                )}
+              </div>
+          }
+        </div>
+    );
 }
 
 export default App;
--- src/components/anime-card.tsx
+++ src/components/anime-card.tsx
@@ -1,10 +1,10 @@
 import "./anime-card.css";
 
 export function AnimeCard(anime: any) {
-  return (
-    <div className={"card"}>
-      <img className={"card-image"} src={anime.image} />
-      <h6 className={"card-title"}>{anime.title}</h6>
-    </div>
-  )
+    return (
+        <div className={"card"}>
+            <img className={"card-image"} src={anime.image} />
+            <h6 className={"card-title"}>{anime.title}</h6>
+        </div>
+    )
 }
